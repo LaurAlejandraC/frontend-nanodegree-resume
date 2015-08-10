e162c49c42ccb3d3bcaa9d9249fa06b06ef3cfe9
Laura Alejandra Chaparro Gutiérrez <lauralejandracg@gmail.com>
2015-08-10 16:25:59 -0500

    Basic info JSON
    JSON created for Works, Projects, Bio and Education sections of the CV

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,70 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var works = {
+	"jobs" : [
+		{
+			"employer" : "Google Student Ambassador Program",
+			"title" : "Google Student Ambassador Universidad Nacional de Colombia",
+			"location" : "Bogotá",
+			"dates" : "2013",
+			"description" : "GSA Latam"
+		}
+	]
+};
+
+var projects = {
+	"project" : [
+		{
+			"title" : "DB final project",
+			"dates" : "June 2014",
+			"description" : "Final project for DB subject",
+			"images" : ["http://placehold.it/350x150", "http://placehold.it/360x160", "http://placehold.it/450x250"]
+		}
+	]
+};
+
+var bio = {
+	"name" : "Laura Alejandra Chaparro",
+	"role" : "Student",
+	"welcomeMessage" : "This is a welcome message",
+	"contacts" : {
+		"mobile" : "+57 3015869264",
+		"email" : "lachaparrog@unal.edu.co",
+		"github" : "www.github.com/LaurAlejandraC",
+		"location" : "Bogotá"
+	},
+	"skills" : ["JS", "HTML", "Java"]
+};
+
+var education = {
+	"schools" : [
+		{
+			"name" : "Colegio Mayor de San Bartolomé",
+			"location" : "Bogotá",
+			"degree" : "Bachiller",
+			"majors" : "Bachiller Académico",
+			"dates" : "2005-2011",
+			"url" : "http://www.sanbartolome.edu.co/"
+		},
+		{
+			"name" : "Universidad Nacional de Colombia",
+			"location" : "Bogotá",
+			"degree" : "BA",
+			"majors" : "Ingeniería de Sistemas y Computación",
+			"dates" : "2012 - ",
+			"url" : "www.unal.edu.co"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "Intro to HTML and CSS",
+			"school" : "Udacity",
+			"dates" : "August 2015",
+			"url" : "https://www.udacity.com/course/viewer#!/c-ud304"
+		},
+		{
+			"title" : "JavaScript Basics",
+			"school" : "Udacity",
+			"dates" : "August 2015",
+			"url" : "https://www.udacity.com/course/viewer#!/c-ud804/"
+		}
+	]
+};
